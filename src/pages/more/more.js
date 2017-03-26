6cb2914a04f7dcadd037f3017640f0dc203535de
yewenjuan <13197008000@qq.com>
2017-03-26 15:40:15 +0800

    wenjuan's changed

--- src/pages/more/more.js
+++ src/pages/more/more.js
@@ -1,8 +1,15 @@
 export default {
-  name: 'More',
+  name: 'page-switch',
   data () {
     return {
-      msg: 'Welcome to More'
+      msg: 'Welcome to More',
+      value: '已开启',
+      infoValue: '已开启'
+    }
+  },
+  methods: {
+    handleChange(event) {
+      console.log(event)
     }
   }
 }
